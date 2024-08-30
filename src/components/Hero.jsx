a7e38dcc1237d9c1fb778e859f076de24efb2d4f
hzqula <ilooqstrasi@gmail.com>
2024-08-30 18:58:51 +0700

    fix: change the container height to h-dvh, and height of main element to h-full

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,10 +30,10 @@ const Hero = ({ id }) => {
   };
 
   return (
-    <section className="relative w-full h-screen overflow-hidden snap-start snap-always">
+    <section className="relative w-full overflow-hidden h-dvh snap-start snap-always">
       <main
         id={id}
-        className="flex flex-col items-center justify-center w-[85%] h-dvh mx-auto lg:w-4/5 "
+        className="flex flex-col items-center justify-center w-[85%] h-full mx-auto lg:w-4/5 "
       >
         <div className="flex flex-col items-center justify-center w-full lg:mt-20">
           <div className="flex flex-col items-center justify-center gap-4">
